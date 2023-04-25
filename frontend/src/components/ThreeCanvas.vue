147f9ba388f4e0fa9c4fcebef643b65df477efd7
GabrielMayrand <gabmayrand@hotmail.ca>
2023-04-25 00:04:04 -0400

    feat: BasicMap

--- frontend/src/components/ThreeCanvas.vue
+++ frontend/src/components/ThreeCanvas.vue
@@ -2,14 +2,12 @@
     <TresCanvas clear-color="#82DBC5" window-size>
         <TresPerspectiveCamera />
         <OrbitControls/>
-        <TresMesh>
-            <TresBoxGeometry :args="[1, 1, 1]" />
-            <TresMeshBasicMaterial color="orange" />
-        </TresMesh>
-        <TresMesh :position="[1, 0, 0]" >
-            <TresBoxGeometry :args="[1, 1, 1]"/>
-            <TresMeshBasicMaterial color="red"/>
-        </TresMesh>
+        <TresScene>
+            <TresMesh v-for="(cube, index) in map" :key="index" :position="cube.position">
+                <TresBoxGeometry :args="cube.dimensions"/>
+                <TresMeshBasicMaterial :color="cube.color"/>
+            </TresMesh>
+        </TresScene>
         <TresAmbientLight :intensity="1"/>
     </TresCanvas>
 </template>
@@ -23,6 +21,43 @@ export default {
     components:{
         TresCanvas: TresCanvas,
         OrbitControls: OrbitControls
+    },
+    data(){
+        return{
+          map:[
+            {position: [0, 1, 2], color: 'orange', dimensions: [1, 1, 1]},
+
+            {position: [0, 0, 0], color: 'grey', dimensions: [1, 1, 1]},
+            {position: [1, 0, 0], color: 'grey', dimensions: [1, 1, 1]},
+            {position: [2, 0, 0], color: 'grey', dimensions: [1, 1, 1]},
+            {position: [3, 0, 0], color: 'grey', dimensions: [1, 1, 1]},
+            {position: [4, 0, 0], color: 'grey', dimensions: [1, 1, 1]},
+
+            {position: [0, 0, 1], color: 'black', dimensions: [1, 1, 1]},
+            {position: [1, 0, 1], color: 'black', dimensions: [1, 1, 1]},
+            {position: [2, 0, 1], color: 'black', dimensions: [1, 1, 1]},
+            {position: [3, 0, 1], color: 'black', dimensions: [1, 1, 1]},
+            {position: [4, 0, 1], color: 'black', dimensions: [1, 1, 1]},
+
+            {position: [0, 0, 2], color: 'black', dimensions: [1, 1, 1]},
+            {position: [1, 0, 2], color: 'yellow', dimensions: [1, 1, 1]},
+            {position: [2, 0, 2], color: 'yellow', dimensions: [1, 1, 1]},
+            {position: [3, 0, 2], color: 'black', dimensions: [1, 1, 1]},
+            {position: [4, 0, 2], color: 'yellow', dimensions: [1, 1, 1]},
+
+            {position: [0, 0, 3], color: 'black', dimensions: [1, 1, 1]},
+            {position: [1, 0, 3], color: 'black', dimensions: [1, 1, 1]},
+            {position: [2, 0, 3], color: 'black', dimensions: [1, 1, 1]},
+            {position: [3, 0, 3], color: 'black', dimensions: [1, 1, 1]},
+            {position: [4, 0, 3], color: 'black', dimensions: [1, 1, 1]},
+
+            {position: [0, 0, 4], color: 'grey', dimensions: [1, 1, 1]},
+            {position: [1, 0, 4], color: 'grey', dimensions: [1, 1, 1]},
+            {position: [2, 0, 4], color: 'grey', dimensions: [1, 1, 1]},
+            {position: [3, 0, 4], color: 'grey', dimensions: [1, 1, 1]},
+            {position: [4, 0, 4], color: 'grey', dimensions: [1, 1, 1]},
+          ]
+        }
     }
 }
 </script>
