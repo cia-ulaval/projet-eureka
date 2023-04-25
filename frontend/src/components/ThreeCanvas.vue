8d6c3e00f73a4194aee754a494057f2a03df462f
GabrielMayrand <gabmayrand@hotmail.ca>
2023-04-24 22:48:53 -0400

    feature: init 2 basic cubes to test ThreeJs

---
+++ frontend/src/components/ThreeCanvas.vue
@@ -0,0 +1,28 @@
+<template>
+    <TresCanvas clear-color="#82DBC5" window-size>
+        <TresPerspectiveCamera />
+        <OrbitControls/>
+        <TresMesh>
+            <TresBoxGeometry :args="[1, 1, 1]" />
+            <TresMeshBasicMaterial color="orange" />
+        </TresMesh>
+        <TresMesh :position="[1, 0, 0]" >
+            <TresBoxGeometry :args="[1, 1, 1]"/>
+            <TresMeshBasicMaterial color="red"/>
+        </TresMesh>
+        <TresAmbientLight :intensity="1"/>
+    </TresCanvas>
+</template>
+
+<script>
+import {extend, TresCanvas} from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos';
+extend({ OrbitControls })
+export default {
+    name: 'ThreeCanvas',
+    components:{
+        TresCanvas: TresCanvas,
+        OrbitControls: OrbitControls
+    }
+}
+</script>
