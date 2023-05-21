d3a1f4ae35d7033b81d231a243d8a6cb0aa4e728
le-smog <le_smog@gmx.com>
2023-05-21 15:48:43 -0400

    feat: car control enforcement and texturing

--- frontend/src/components/ThreeCanvas.vue
+++ frontend/src/components/ThreeCanvas.vue
@@ -1,13 +1,24 @@
 <script setup>
 import {useFBX} from '@tresjs/cientos'
 import {shallowRef} from "vue";
-import {useRenderLoop} from "@tresjs/core";
+import {useRenderLoop, useTexture} from "@tresjs/core";
 
-// const CUBE_SIZE = 400
+
+const upDownTexture = await useTexture({
+    map: '/textures/upDown.jpg'
+});
+const leftRightTexture = await useTexture({
+    map: '/textures/leftRight.jpg'
+});
+const otherTexture = await useTexture({
+    map: '/textures/otherIntersection.jpg'
+});
+const grassTexture = await useTexture({
+    map: '/textures/grass.jpg'
+});
+
 
 const model = await useFBX('/models/Car.fbx')
-
-// const tensorFile = require('../data/tensor.json')
 
 const boxRef = shallowRef(null)
 const intersectionRef = shallowRef(null)
@@ -18,6 +29,12 @@ let carPosition = [0, 0]
 let carOrientation = "up"
 
 function moveMap(delta) {
+    const rowCol = getTensorPosition()
+    const row = rowCol[0]
+    const col = rowCol[1]
+
+    const speed = speedTensor[row][col] ** 2 / 5
+
     let dx = 0
     let dy = 0
     switch (carOrientation) {
@@ -35,53 +52,163 @@ function moveMap(delta) {
             break;
     }
 
-    let distance = 1000*delta
+    let distance = speed * delta
 
     for (let i = 0; i < boxRef.value.length; i++) {
         let cube = boxRef.value[i];
-        cube.position.z -= dy*distance
-        cube.position.x -= dx*distance
+        cube.position.z -= dy * distance
+        cube.position.x -= dx * distance
     }
     for (let i = 0; i < intersectionRef.value.length; i++) {
         let cube = intersectionRef.value[i];
-        cube.position.z -= dy*distance;
-        cube.position.x -= dx*distance;
+        cube.position.z -= dy * distance;
+        cube.position.x -= dx * distance;
     }
-    carPosition[0] += dx*distance
-    carPosition[1] += dy*distance
+    carPosition[0] += dx * distance
+    carPosition[1] += dy * distance
+    return distance
 }
 
 function isNextCubeAnIntersection() {
     // according to the car position and orientation and the tensor, check if the next cube is an intersection, i.e one of the 3 values is 1
     // if it is, return true, else return false
-    let row = Math.round(carPosition[0] / CUBE_SIZE);
-    let col = Math.round(carPosition[1] / CUBE_SIZE);
+    let rowCol = getTensorPosition()
+    let row = rowCol[0]
+    let col = rowCol[1]
 
     switch (carOrientation) {
         case "up":
-            return tensor[row][col + 1] === 1 || tensor[row][col - 1] === 1 || tensor[row - 1][col] === 1;
+            if (row === 0) {
+                if (tensor[row + 1] === undefined) {
+                    return false;
+                }
+                return tensor[row + 1][col] === 1;
+            }
+            if (tensor[row + 1] === undefined) {
+                return tensor[row - 1][col] === 1;
+            }
+            return tensor[row + 1][col] === 1 || tensor[row - 1][col] === 1;
         case "down":
-            return tensor[row][col + 1] === 1 || tensor[row][col - 1] === 1 || tensor[row + 1][col] === 1;
+            if (row === 0) {
+                if (tensor[row + 1] === undefined) {
+                    return false;
+                }
+                return tensor[row + 1][col] === 1;
+            }
+            if (tensor[row + 1] === undefined) {
+                return tensor[row - 1][col] === 1;
+            }
+            return tensor[row + 1][col] === 1 || tensor[row - 1][col] === 1;
         case "left":
-            return tensor[row][col - 1] === 1 || tensor[row + 1][col] === 1 || tensor[row - 1][col] === 1;
+            if (col === 0) {
+                return tensor[row][col + 1] === 1;
+            }
+            return tensor[row][col + 1] === 1 || tensor[row][col - 1] === 1;
         case "right":
-            return tensor[row][col + 1] === 1 || tensor[row + 1][col] === 1 || tensor[row - 1][col] === 1;
+            if (col === 0) {
+                return tensor[row][col + 1] === 1;
+            }
+            return tensor[row][col + 1] === 1 || tensor[row][col - 1] === 1;
     }
 }
 
-onLoop(({delta}) => {
-    if (boxRef.value) {
-        if (isNextCubeAnIntersection()) {
-            if(nextDirection !== null) {
-                carOrientation = nextDirection
-                moveMap(delta)
-                console.log(carOrientation)
-            }
+function getAvailableTurns() {
+    // according to the car position and orientation and the tensor, return the available turns
+    // if there is no turn, return null
+    let rowCol = getTensorPosition()
+    let row = rowCol[0]
+    let col = rowCol[1]
+
+    let turns = []
+
+    if (col !== 0) {
+        if (tensor[row][col - 1] === 1) {
+            turns.push("down")
         }
-        else {
-            nextDirection = null
-            moveMap(delta)
-            console.log(carOrientation)
+    }
+    if (tensor[row][col + 1] !== undefined) {
+        if (tensor[row][col + 1] === 1) {
+            turns.push("up")
+        }
+    }
+    if (row !== 0) {
+        if (tensor[row - 1][col] === 1) {
+            turns.push("right")
+        }
+    }
+    if (tensor[row + 1] !== undefined) {
+        if (tensor[row + 1][col] === 1) {
+            turns.push("left")
+        }
+    }
+
+    return turns
+}
+
+function getTensorPosition() {
+    let row = Math.round(carPosition[0] / CUBE_SIZE);
+    let col = Math.round(carPosition[1] / CUBE_SIZE);
+    return [row, col]
+}
+
+let state = "moving"
+let cubeToCross = null
+let toDirection = null
+let distanceToCross = null
+let path = []
+
+// TODO : Ajouter tous les points en coordonnée tenseur aux path (ne pas dupliquer précedent)
+// TODO: change base car position
+// TODO: caller un truc lorsque la voiture est sur la fin
+
+onLoop(({delta}) => {
+    console.log("state=" + state + " - toDirection=" + toDirection + " - carOrientation=" + carOrientation)
+    if (boxRef.value) {
+        switch (state) {
+            case "moving":
+                if (isNextCubeAnIntersection()) {
+                    state = "stopped"
+                    cubeToCross = getTensorPosition()
+                    nextDirection = null
+                    break;
+                } else {
+                    moveMap(delta)
+                    break;
+                }
+            case "stopped":
+                if (nextDirection !== null && getAvailableTurns().includes(nextDirection)) {
+                    console.log("availableTurns=" + getAvailableTurns())
+                    state = "crossing"
+                    toDirection = nextDirection
+                    break;
+                } else {
+                    break;
+                }
+            case "crossing":
+                if (cubeToCross !== null && cubeToCross[0] === getTensorPosition()[0] && cubeToCross[1] === getTensorPosition()[1]) {
+                    if (distanceToCross === null) {
+                        distanceToCross = CUBE_SIZE
+                    } else if (distanceToCross > CUBE_SIZE / 2) {
+                        distanceToCross -= moveMap(delta)
+                        break;
+                    } else if (distanceToCross > 0) {
+                        if (carOrientation !== toDirection) {
+                            carOrientation = toDirection
+                            path.push(toDirection)
+                        }
+                        distanceToCross -= moveMap(delta)
+                        break;
+                    } else {
+                        // state = "moving"
+                        distanceToCross = null
+                        break;
+                    }
+                    break;
+                } else {
+                    distanceToCross = null
+                    state = "moving"
+                    break;
+                }
         }
     }
 })
@@ -98,22 +225,28 @@ onLoop(({delta}) => {
         </div>
         <TresCanvas clear-color="#82DBC5" window-size="true" class="canvas">
             <TresPerspectiveCamera :position="[0, 300, -800]" :look-at="mapCenter" :far="100000"/>
-        <OrbitControls/>
+            <OrbitControls/>
             <TresScene>
                 <TresMesh v-for="(cube, index) in map" :key="index" :position="cube.position" ref="boxRef">
                     <TresBoxGeometry :args="cube.dimensions"/>
-                    <TresMeshBasicMaterial :color="cube.color"/>
+
+                    <TresMeshStandardMaterial v-if="cube.map === 'upDown'" :map="upDownTexture.map"/>
+                    <TresMeshStandardMaterial v-else-if="cube.map === 'leftRight'" :map="leftRightTexture.map"/>
+                    <TresMeshStandardMaterial v-else-if="cube.map === 'other'" :map="otherTexture.map"/>
+                    <TresMeshStandardMaterial v-else-if="cube.map === 'grass'" :map="grassTexture.map"/>
+                    <TresMeshStandardMaterial v-else :map="grassTexture.map"/>
                 </TresMesh>
-                <TresMesh v-for="(cube, index) in intersection" :key="index" :position="cube.position" ref="intersectionRef">
+                <TresMesh v-for="(cube, index) in intersection" :key="index" :position="cube.position"
+                          ref="intersectionRef">
                     <TresBoxGeometry :args="cube.dimensions"/>
                     <TresMeshBasicMaterial :color="cube.color"/>
                 </TresMesh>
-            <Suspense>
-                <TresMesh v-bind="model"/>
-            </Suspense>
-        </TresScene>
+                <Suspense>
+                    <TresMesh v-bind="model"/>
+                </Suspense>
+            </TresScene>
             <TresAmbientLight/>
-        map.length/2
+            map.length/2
         </TresCanvas>
     </div>
 </template>
@@ -131,14 +264,15 @@ console.log(nextDirection)
 import tensor from '../data/tensor.json'
 import speedTensor from '../data/speedTensor.json'
 import stopTensor from '../data/stopTensor.json'
+
 export default {
     name: 'ThreeCanvas',
     components: {
         OrbitControls: OrbitControls,
         TresCanvas: TresCanvas,
     },
-    data(){
-        return{
+    data() {
+        return {
             mapCenter: [15, 1, 13],
             car: {
                 position: [15, 1, 13],
@@ -153,7 +287,36 @@ export default {
         }
     },
     methods: {
-        move(direction){
+        robustGet(map, row, col = null) {
+            if (row < 0) {
+                return null
+            }
+            if (col < 0) {
+                return null
+            }
+            if (map[row] === undefined) {
+                return null
+            }
+            if (map[row][col] === undefined) {
+                return null
+            }
+            return map[row][col]
+        },
+        findAppropriateMap(tensorCoordinate) {
+            const row = tensorCoordinate[0];
+            const col = tensorCoordinate[1];
+
+            // if the cube at the right and the left are 1, and up and down are 0, then it's a left-right cube
+            if (this.robustGet(this.tensor, row, col - 1) === 1 && this.robustGet(this.tensor, row, col + 1) === 1 && this.robustGet(this.tensor, row - 1, col) !== 1 && this.robustGet(this.tensor, row + 1, col) !== 1) {
+                return 'upDown'
+            }
+            // if the cube at the right and the left are 0, and up and down are 1, then it's a up-down cube
+            if (this.robustGet(this.tensor, row, col - 1) !== 1 && this.robustGet(this.tensor, row, col + 1) !== 1 && this.robustGet(this.tensor, row - 1, col) === 1 && this.robustGet(this.tensor, row + 1, col) === 1) {
+                return 'leftRight'
+            }
+            return 'other'
+        },
+        move(direction) {
             switch (direction) {
                 case 'up':
                     nextDirection = 'up'
@@ -171,7 +334,7 @@ export default {
                     break;
             }
         },
-        findNextIntersection(){
+        findNextIntersection() {
             const row = this.car.position[0];
             const col = this.car.position[2];
             const intersections = this.intersection;
@@ -203,8 +366,16 @@ export default {
             let map = []
             for (let i = 0; i < this.tensor.length; i++) {
                 for (let j = 0; j < this.tensor[i].length; j++) {
-                    if (this.tensor[i][j] === 1) map.push({position: [i*CUBE_SIZE, -CAR_SIZE, j*CUBE_SIZE], color: 'black', dimensions: [CUBE_SIZE, CUBE_SIZE, CUBE_SIZE]})
-                    if (this.tensor[i][j] === 0) map.push({position: [i*CUBE_SIZE, -CAR_SIZE, j*CUBE_SIZE], color: '#41980A', dimensions: [CUBE_SIZE, CUBE_SIZE, CUBE_SIZE]})
+                    if (this.tensor[i][j] === 1) map.push({
+                        position: [i * CUBE_SIZE, -CAR_SIZE, j * CUBE_SIZE],
+                        map: this.findAppropriateMap([i, j]),
+                        dimensions: [CUBE_SIZE, CUBE_SIZE, CUBE_SIZE]
+                    })
+                    if (this.tensor[i][j] === 0) map.push({
+                        position: [i * CUBE_SIZE, -CAR_SIZE, j * CUBE_SIZE],
+                        map: 'grass',
+                        dimensions: [CUBE_SIZE, CUBE_SIZE, CUBE_SIZE]
+                    })
                 }
             }
             this.map = map
@@ -213,8 +384,16 @@ export default {
             let intersection = []
             for (let i = 0; i < this.stopTensor.length; i++) {
                 for (let j = 0; j < this.stopTensor[i].length; j++) {
-                    if (this.stopTensor[i][j] === 1) intersection.push({position: [i*CUBE_SIZE, CUBE_SIZE-CAR_SIZE, j*CUBE_SIZE], color: 'red', dimensions: [0.3*CUBE_SIZE, 0.3*CUBE_SIZE, 0.3*CUBE_SIZE]})
-                    if (this.stopTensor[i][j] === 2) intersection.push({position: [i*CUBE_SIZE, CUBE_SIZE-CAR_SIZE, j*CUBE_SIZE], color: 'blue', dimensions: [0.3*CUBE_SIZE, 0.3*CUBE_SIZE, 0.3*CUBE_SIZE]})
+                    if (this.stopTensor[i][j] === 1) intersection.push({
+                        position: [i * CUBE_SIZE, CUBE_SIZE - CAR_SIZE, j * CUBE_SIZE],
+                        color: 'red',
+                        dimensions: [0.3 * CUBE_SIZE, 0.3 * CUBE_SIZE, 0.3 * CUBE_SIZE]
+                    })
+                    if (this.stopTensor[i][j] === 2) intersection.push({
+                        position: [i * CUBE_SIZE, CUBE_SIZE - CAR_SIZE, j * CUBE_SIZE],
+                        color: 'blue',
+                        dimensions: [0.3 * CUBE_SIZE, 0.3 * CUBE_SIZE, 0.3 * CUBE_SIZE]
+                    })
                 }
             }
             this.intersection = intersection
@@ -232,11 +411,13 @@ export default {
 img {
     filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(354deg) brightness(102%) contrast(101%);
 }
-.container{
+
+.container {
     position: relative;
     left: 0;
     top: 0;
 }
+
 .canvas {
     width: 100%;
     height: 100%;
@@ -245,19 +426,21 @@ img {
     top: 0;
     z-index: 1;
 }
+
 .arrows {
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100px;
     height: 100px;
-    background-color: rgba(2,0,36,0.3);
+    background-color: rgba(2, 0, 36, 0.3);
     border: 1px solid #ccc;
     border-radius: 5px;
     font-size: 24px;
     font-weight: bold;
     cursor: pointer;
 }
+
 .gamePad {
     transform: rotate(-55deg);
     position: absolute;
@@ -272,17 +455,21 @@ img {
     ". up ."
     "left down right";
 }
+
 .up {
     grid-area: up;
 }
+
 .left {
     transform: rotate(-90deg);
     grid-area: left;
 }
+
 .down {
     transform: rotate(180deg);
     grid-area: down;
 }
+
 .right {
     transform: rotate(90deg);
     grid-area: right;
