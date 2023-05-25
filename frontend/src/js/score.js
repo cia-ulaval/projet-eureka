e6a491a23da0bfaf3d70faa08ba254f0abfe6920
Sabrina Cazzolato <sabrina.cazzolato.1@ulaval.ca>
2023-05-24 21:12:39 -0400

    fix: post score url

--- frontend/src/js/score.js
+++ frontend/src/js/score.js
@@ -2,10 +2,9 @@ import axios from 'axios';
 
 
 export async function getScore(path) {
-    const url = `${axios.defaults.baseURL}/score`;
+    const url = `${axios.defaults.baseURL}score`;
     const data = {
         path: path
     };
-    console.log(path, data)
     return axios.post(url, data).then((response) => response.data);
 }
